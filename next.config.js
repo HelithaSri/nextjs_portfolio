dc8af63ca9555c82143ea1aa4c088239cb572c05
HelithaSri <helitha.pravin@gmail.com>
2023-07-03 21:54:34 +0530

    added base path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // comment on these lines before running locally
     output: 'export',
+    basePath: '/nextjs_portfolio'
 }
 
 module.exports = nextConfig
