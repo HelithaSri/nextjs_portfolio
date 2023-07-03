7b9279a11695f811833d48b279cb1141b5a5923d
HelithaSri <helitha.pravin@gmail.com>
2023-07-03 23:19:03 +0530

    test configs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = true;
+
 const nextConfig = {
     // comment on these lines before running locally
     // output: 'export',
     // basePath: '/_next',
-    assetPrefix:'.',
+    assetPrefix: isProd ? 'nextjs_portfolio/' : '',
+
 }
 
 module.exports = nextConfig
